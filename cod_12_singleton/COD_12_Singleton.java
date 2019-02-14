399a2985c30d873ee3053be92d743a1de85a5417
fsancheztemprano <fsancheztemprano@danielcastelao.org>
2019-02-14 12:01:58 +0100

    Singleton

--- cod_12_singleton/COD_12_Singleton.java
+++ cod_12_singleton/COD_12_Singleton.java
@@ -3,7 +3,15 @@ package cod_12_singleton;
 public class COD_12_Singleton {
     public static void main(String[] args) {
         
-
+            Singleton objetoA = Singleton.getInstance("F", 10);
+            objetoA.printSingleton();
+            Singleton objetoB = Singleton.getInstance("S", 11);
+            objetoB.printSingleton();
+            
+            objetoB.setNombre("T");
+            objetoB.setNumero(11);
+            objetoA.printSingleton();
+            objetoB.printSingleton();
 
     }
 }
